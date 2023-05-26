f74d5f844967bf03e3aa1e7084a5b640e2ac5b90
deepoa <deepeaaa45@gmail.com>
2023-05-26 17:42:51 +0530

    personal portfoliio

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ const checkbox = document.getElementById("checkbox");
 checkbox.addEventListener("change", () => {
   document.body.classList.toggle("light");
 });
+
+function typeWriterEffect(ele, text, delay = 150) {
+  console.log(text.length);
+  for (let i = 0; i < text.length; i++) {
+    console.log(text[i]);
+    setTimeout(() => {
+      ele.innerHTML += text[i];
+    }, delay * i);
+  }
+}
+const para = document.getElementById("LastPara");
+
+typeWriterEffect(para, "And I am a Full Stack Developer");
